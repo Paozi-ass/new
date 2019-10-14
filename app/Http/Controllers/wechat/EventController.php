08c7eb11da99dfe1f112cee11270072ac163b12b
Paozi-ass <yxl19970407@163.com>
2019-10-14 16:51:16 +0800

    aa

---
+++ app/Http/Controllers/wechat/EventController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\wechat;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class EventController extends Controller
+{
+    
+    public function event()
+    {
+
+        echo $_GET['echostr'];
+
+    }
+}
