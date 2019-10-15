2d54420d94661f4f0c012f649f1958595b510576
Paozi-ass <yxl19970407@163.com>
2019-10-15 08:51:00 +0800

    aa

--- app/Http/Controllers/wechat/EventController.php
+++ app/Http/Controllers/wechat/EventController.php
@@ -11,7 +11,17 @@ class EventController extends Controller
     public function event()
     {
 
-        echo $_GET['echostr'];
+        // echo $_GET['echostr'];
+        // dd (storage_path());
+        $info = file_get_contents("php://input");
+        // dd($info);
+        file_put_contents(storage_path('logs/wechat/'.date('Y-m-d').'.log'),"<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n",FILE_APPEND);
+        file_put_contents(storage_path('logs/wechat/'.date('Y-m-d').'.log'),$info,FILE_APPEND);
+        file_put_contents(storage_path('logs/wechat/'.date('Y-m-d').'.log'),"<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n    ",FILE_APPEND);
+        
+        $xml_obj = simplexml_load_string($info,'SimpleXMLElement',LIBXML_NOCDATA);
+        $xml_arr = (array)$xml_obj;
+        dd($xml_arr);
 
     }
 }
